3b92ac19fbf1d8f314214fb3029b71a7f1e71521
hideo54 <hideo54@hideo54.com>
2020-09-06 20:31:16 +0900

    Write basic programs

---
+++ index.ts
@@ -0,0 +1,45 @@
+import Twitter from 'twitter';
+import schedule from 'node-schedule';
+import moment from 'moment';
+import { promises as fs } from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import schedules, { Schedule } from './schedules';
+
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY!,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET!,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY!,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET!,
+});
+
+const tweet = async (client: Twitter, schedule: Schedule) => {
+    const mediaIdStrings = schedule.images && await Promise.all(
+        schedule.images.map(imagePath =>
+            fs.readFile(imagePath).then(media =>
+                client.post('media/upload', { media }).then(res =>
+                    res.media_id_string as string
+                )
+            )
+        )
+    );
+    try {
+        await client.post('statuses/update', {
+            status: schedule.text,
+            media_ids: mediaIdStrings?.join(','),
+        });
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+schedule.scheduleJob('*/5 * * * *', async () => {
+    const nowString = moment().format('YYYY-MM-DD HH:mm');
+    const matchingSchedules = schedules.filter(schedule =>
+        schedule.date === nowString
+    );
+    for (const matchingSchedule of matchingSchedules) {
+        await tweet(client, matchingSchedule);
+    }
+});
---
+++ schedules.ts
@@ -0,0 +1,23 @@
+import { stripIndent } from 'common-tags';
+
+export interface Schedule {
+    date: string;
+    text: string;
+    images?: string[];
+    changeIconTo?: string;
+}
+
+const schedules: Schedule[] = [
+    {
+        date: '2020-09-06 20:30',
+        text: stripIndent`
+            テストツイート
+            にひひ
+        `,
+        images: [
+            'media/allyl-eigen.jpg',
+        ],
+    },
+];
+
+export default schedules;
